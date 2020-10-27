7f73dc7bec0214b2981d55e747d12b709887b6d6
Damian Kalka <kalka762@gmail.com>
2020-10-27 13:10:00 +0100

    change url API links to https

--- src/js/API/URL.js
+++ src/js/API/URL.js
@@ -1,4 +1,4 @@
 const API_key = '384dc2376918463fa8b93907200110';
-export const base_URL = `http://api.weatherapi.com/v1/current.json?key=${API_key}`;
-export const base_URLsearch = `http://api.weatherapi.com/v1/search.json?key=${API_key}`;
-export const base_URLforecast = `http://api.weatherapi.com/v1/forecast.json?key=${API_key}`;
+export const base_URL = `https://api.weatherapi.com/v1/current.json?key=${API_key}`;
+export const base_URLsearch = `https://api.weatherapi.com/v1/search.json?key=${API_key}`;
+export const base_URLforecast = `https://api.weatherapi.com/v1/forecast.json?key=${API_key}`;
